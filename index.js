c367f2130482484953b954927837580691afa3ad
yashwanth <yashwanth.kinnu123@gmail.com>
2025-10-11 18:00:31 +0530

    updated-package-json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
-const sqlite3 = require('better-sqlite3');
+const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
